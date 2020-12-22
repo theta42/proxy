2279918ae305edb56d3cef4541258a704151e3ca
William Mantly <wmantly@gmail.com>
2020-12-21 21:31:33 -0500

    backed tracked some changes

--- nodejs/routes/index.js
+++ nodejs/routes/index.js
@@ -25,16 +25,4 @@ router.get('/login', function(req, res, next) {
   res.render('login', {redirect: req.query.redirect});
 });
 
-router.get('/lookup/:host', async function(req, res, next){
-	try{
-		return res.json({
-			string: req.params.host,
-			results: await Host.lookUp(req.params.host),
-		});
-
-	}catch(error){
-		return next(error);
-	}
-});
-
 module.exports = router;
