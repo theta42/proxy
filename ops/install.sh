3094b58a47969b609e08055726069d4c67da2f40
William Mantly <wmantly@gmail.com>
2021-03-15 14:19:05 -0400

    Update install.sh

--- ops/install.sh
+++ ops/install.sh
@@ -4,6 +4,8 @@ apt install libpam0g-dev build-essential redis-server luarocks --no-install-reco
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+echo "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/openresty.list
+    
 wget -O - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
 
 sudo apt-get update
