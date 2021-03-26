815d8f544b9446f28f10f9dc7d9e34d9e8e392d3
William Mantly <wmantly@gmail.com>
2021-03-26 17:08:48 -0400

    converted redis stuff to ES6 classes

--- nodejs/models/user_redis.js
+++ nodejs/models/user_redis.js
@@ -1,14 +1,13 @@
 'use strict';
 
-const objValidate = require('../utils/object_validate');
+const Table = require('../utils/redis_model');
 const {Token, InviteToken} = require('./token');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-const User = require('../utils/redis_model')({
-	_name: 'user',
-	_key: 'username',
-	_keyMap: {
+class User extends Table{
+	static _key = 'username';
+	static _keyMap = {
 		'created_by': {isRequired: true, type: 'string', min: 3, max: 500},
 		'created_on': {default: function(){return (new Date).getTime()}},
 		'updated_by': {default:"__NONE__", isRequired: false, type: 'string',},
@@ -17,102 +16,103 @@ const User = require('../utils/redis_model')({
 		'password': {isRequired: true, type: 'string', min: 3, max: 500},
 		'backing': {default:"redis", isRequired: false, type: 'string',},
 	}
-});
 
-User.backing = "redis";
+	static backing = 'redis'
 
-
-User.add = async function(data) {
-	try{
-		data['password'] = await bcrypt.hash(data['password'], saltRounds);
-		data['backing'] = data['backing'] || 'redis';
+	static async add(data) {
+		try{
+			data['password'] = await bcrypt.hash(data['password'], saltRounds);
+			data['backing'] = data['backing'] || 'redis';
 
 
-		return this.__proto__.add(data);
+		return await super.add(data)
 
-	}catch(error){
-		throw error;
+		}catch(error){
+			throw error;
+		}
 	}
-};
 
-User.addByInvite = async function(data){
-	try{
-		let token = await InviteToken.get(data.token);
+	static async addByInvite(data){
+		try{
+			let token = await InviteToken.get(data.token);
 
-		if(!token.is_valid){
-			let error = new Error('Token Invalid');
-			error.name = 'Token Invalid';
-			error.message = `Token is not valid or as allready been used. ${data.token}`;
-			error.status = 401;
+			if(!token.is_valid){
+				let error = new Error('Token Invalid');
+				error.name = 'Token Invalid';
+				error.message = `Token is not valid or as allready been used. ${data.token}`;
+				error.status = 401;
+				throw error;
+			}
+
+			let user = await this.add(data);
+
+			if(user){
+				await token.consume({claimed_by: user.username});
+				return user;
+			}
+
+		}catch(error){
 			throw error;
 		}
 
-		let user = await this.add(data);
+	};
 
-		if(user){
-			await token.consume({claimed_by: user.username});
-			return user;
+	async setPassword(data){
+		try{
+			data['password'] = await bcrypt.hash(data['password'], saltRounds);
+
+			return this.update(data);
+		}catch(error){
+			throw error;
 		}
-
-	}catch(error){
-		throw error;
 	}
 
-};
+	async invite(){
+		try{
+			let token = await InviteToken.add({created_by: this.username});
+			
+			return token;
 
-User.setPassword = async function(data){
-	try{
-		data['password'] = await bcrypt.hash(data['password'], saltRounds);
-
-		return this.__proto__.update(data);
-	}catch(error){
-		throw error;
-	}
-};
-
-User.invite = async function(){
-	try{
-		let token = await InviteToken.add({created_by: this.username});
-		
-		return token;
-
-	}catch(error){
-		throw error;
-	}
-};
-
-User.login = async function(data){
-	try{
-		let user = await User.get(data);
-
-		let auth = await bcrypt.compare(data.password, user.password);
-
-		if(auth){
-			return user
-		}else{
-			throw this.errors.login();
+		}catch(error){
+			throw error;
 		}
-	}catch(error){
-		if (error == 'Authentication failure'){
-			throw this.errors.login()
-		}
-		throw error;
 	}
-};
+
+	static async login(data){
+		try{
+
+			let user = await User.get(data);
+			let auth = await bcrypt.compare(data.password, user.password);
+
+			if(auth){
+				return user
+			}else{
+				throw this.errors.login();
+			}
+		}catch(error){
+			if (error == 'Authentication failure'){
+				throw this.errors.login()
+			}
+			throw error;
+		}
+	};
+
+
+}
 
 module.exports = {User};
 
 
 (async function(){
 	try{
-		await User.get('proxyadmin1');
-		console.info('proxyadmin1 user exists');
+		await User.get('proxyadmin');
+		console.info('proxyadmin user exists');
 	}catch(error){
 		try{
 			let user = await User.add({
-				username:'proxyadmin1',
-				password: 'proxyadmin1',
-				created_by:'proxyadmin1'
+				username:'proxyadmin',
+				password: 'proxyadmin123',
+				created_by:'proxyadmin'
 			});
 			console.log('proxyadmin1 created', user);	
 		}catch(error){
--- nodejs/utils/redis_model.js
+++ nodejs/utils/redis_model.js
@@ -4,187 +4,162 @@ const client = require('../utils/redis');
 const objValidate = require('../utils/object_validate');
 
 
-let table = {};
-
-table.get = async function(data){
-	try{
-		// if the data argument was passed as the index key value, make a data
-		// object and add the index key to it.
-		if(typeof data !== 'object'){
-			let key = data;
-			data = {};
-			data[this._key] = key;
+class Table{
+	constructor(data){
+		for(let key in data){
+			this[key] = data[key];
 		}
+	}
 
-		// Get all the hash keys for the passed index key.
-		let res = await client.HGETALL(`${this._name}_${data[this._key]}`);
+	static async get(index){
+		try{
 
-		// If the redis query resolved to something, prepare the data.
-		if(res){
+			if(typeof index === 'object'){
+				index = index[this._key]
+			}
+
+			let result = await client.HGETALL(`${this.prototype.constructor.name}_${index}`);
+
+			if(!result){
+				let error = new Error('EntryNotFound');
+				error.name = 'EntryNotFound';
+				error.message = `${this.prototype.constructor.name}:${index} does not exists`;
+				error.status = 404;
+				throw error;
+			}
 
 			// Redis always returns strings, use the keyMap schema to turn them
 			// back to native values.
-			res = objValidate.parseFromString(this._keyMap, res);
+			result = objValidate.parseFromString(this._keyMap, result);
 
-			// Make sure the index key in in the returned object.
-			res[this._key] = data[this._key];
+			return new this.prototype.constructor(result)
 
-			// Create a instance for this redis entry.
-			var entry = Object.create(this);
-
-			// Insert the redis response into the instance.
-			Object.assign(entry, res);
-
-			// Return the instance to the caller.
-			return entry;
-		}
-
-	}catch(error){
-		throw error
-	}
-
-	let error = new Error('EntryNotFound');
-	error.name = 'EntryNotFound';
-	error.message = `${this._name}:${data[this._key]} does not exists`;
-	error.status = 404;
-	throw error;
-};
-
-table.exists = async function(data){
-	// Return true or false if the requested entry exists ignoring error's.
-	try{
-		await this.get(data);
-
-		return true
-	}catch(error){
-		return false;
-	}
-};
-
-table.list = async function(){
-	// return a list of all the index keys for this table.
-	try{
-
-		return await client.SMEMBERS(this._name);
-
-	}catch(error){
-		throw error;
-	}
-};
-
-table.listDetail = async function(){
-	// Return a list of the entries as instances.
-	let out = [];
-
-	for(let entry of await this.list()){
-		out.push(await this.get(entry));
-	}
-
-	return out
-};
-
-table.add = async function(data, noMemberAdd){
-	// Add a entry to this redis table.
-	try{
-
-		// Validate the passed data by the keyMap schema.
-
-		data = objValidate.processKeys(this._keyMap, data);
-
-		// Do not allow the caller to overwrite an existing index key,
-		if(data[this._key] && await this.exists(data)){
-			let error = new Error('EntryNameUsed');
-			error.name = 'EntryNameUsed';
-			error.message = `${this._name}:${data[this._key]} already exists`;
-			error.status = 409;
-
+		}catch(error){
 			throw error;
 		}
 
-		// Add the key to the members for this redis table
-		if(!noMemberAdd) await client.SADD(this._name, data[this._key]);
+	}
 
-		// Add the values for this entry.
-		for(let key of Object.keys(data)){
-			await client.HSET(`${this._name}_${data[this._key]}`, key, data[key]);
+	static async exists(index){
+		try{
+			await this.get(data);
+
+			return true
+		}catch(error){
+			return false;
+		}
+	}
+
+	static async list(){
+		// return a list of all the index keys for this table.
+		try{
+			return await client.SMEMBERS(this.prototype.constructor.name);
+
+		}catch(error){
+			throw error;
+		}
+	}
+
+	static async listDetail(){
+		// Return a list of the entries as instances.
+		let out = [];
+
+		for(let entry of await this.list()){
+			out.push(await this.get(entry));
 		}
 
-		// return the created redis entry as entry instance.
-		return await this.get(data[this._key]);
-	} catch(error){
-		throw error;
+		return out
 	}
-};
 
-table.update = async function(data, key){
-	// Update an existing entry.
-	try{
-		// If an index key is passed, we assume is passed, assume we are not
-		// part of an entry instance. Make one and recall this from from a entry
-		// instance,
-		if(key) return await (await this.get(key)).update(data);
+	static async add(data){
+		// Add a entry to this redis table.
+		try{
+			// Validate the passed data by the keyMap schema.
 
-		// Check to see if entry name changed.
-		if(data[this._key] && data[this._key] !== this[this._key]){
+			data = objValidate.processKeys(this._keyMap, data);
 
-			// Merge the current data into with the updated data 
-			let newData = Object.assign({}, this, data);
+			// Do not allow the caller to overwrite an existing index key,
+			if(data[this._key] && await this.exists(data)){
+				let error = new Error('EntryNameUsed');
+				error.name = 'EntryNameUsed';
+				error.message = `${this.prototype.constructor.name}:${data[this._key]} already exists`;
+				error.status = 409;
 
-			// Remove the updated failed so it doesnt keep it
-			delete newData.updated;
-
-			// Create a new record for the updated entry. If that succeeds,
-			// delete the old recored
-			if(await this.add(newData)) await this.remove();
-
-		}else{
-			// Update what ever fields that where passed.
-
-			// Validate the passed data, ignoring required fields.
-			data = objValidate.processKeys(this._keyMap, data, true);
-			
-			// Loop over the data fields and apply them to redis
-			for(let key of Object.keys(data)){
-				this[key] = data[key];
-				await client.HSET(`${this._name}_${this[this._key]}`, key, data[key]);
+				throw error;
 			}
+
+			// Add the key to the members for this redis table
+			await client.SADD(this.prototype.constructor.name, data[this._key]);
+
+			// Add the values for this entry.
+			for(let key of Object.keys(data)){
+				await client.HSET(`${this.prototype.constructor.name}_${data[this._key]}`, key, objValidate.parseToString(data[key]));
+			}
+
+			// return the created redis entry as entry instance.
+			return await this.get(data[this._key]);
+		} catch(error){
+			throw error;
 		}
-
-		return this;
-	
-	} catch(error){
-		// Pass any error to the calling function
-		throw error;
 	}
-};
 
-table.remove = async function(data){
-	// Remove an entry from this table.
+	async update(data, key){
+		// Update an existing entry.
+		try{
+			// Check to see if entry name changed.
+			if(data[this.constructor._key] && data[this.constructor._key] !== this[this.constructor._key]){
 
-	data = data || this;
-	try{
-		// Remove the index key from the tables members list.
-		await client.SREM(this._name, data[this._key]);
+				// Merge the current data into with the updated data 
+				let newData = Object.assign({}, this, data);
 
-		// Remove the entries hash values.
-		let count = await client.DEL(`${this._name}_${data[this._key]}`);
+				// Remove the updated failed so it doesnt keep it
+				delete newData.updated;
 
-		// Return the number of removed values to the caller.
-		return count;
+				// Create a new record for the updated entry. If that succeeds,
+				// delete the old recored
+				if(await this.add(newData)) await this.remove();
 
-	} catch(error) {
-		throw error;
+			}else{
+				// Update what ever fields that where passed.
+
+				// Validate the passed data, ignoring required fields.
+				data = objValidate.processKeys(this.constructor._keyMap, data, true);
+				
+				// Loop over the data fields and apply them to redis
+				for(let key of Object.keys(data)){
+					this[key] = data[key];
+					await client.HSET(`${this.constructor.name}_${this[this.constructor._key]}`, key, data[key]);
+				}
+			}
+
+			return this;
+		
+		} catch(error){
+			// Pass any error to the calling function
+			throw error;
+		}
 	}
-};
 
-function Table(data){
-	// Create a table instance.
-	let instance = Object.create(data);
-	Object.assign(instance, table);
+	async remove(data){
+		// Remove an entry from this table.
 
-	// Return the table instance to the caller.
-	return Object.create(instance);
+		try{
+			// Remove the index key from the tables members list.
 
-};
+			await client.SREM(this.constructor.name, this[this.constructor._key]);
+
+			// Remove the entries hash values.
+			let count = await client.DEL(`${this.constructor.name}_${this[this.constructor._key]}`);
+
+			// Return the number of removed values to the caller.
+			return count;
+
+		} catch(error) {
+			throw error;
+		}
+	};
+
+}
+
 
 module.exports = Table;
