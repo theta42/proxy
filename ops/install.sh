7b274b829bc520baa8b111fc737fc3aca0d936a9
onyxstarwave <143672078+onyxstarwave@users.noreply.github.com>
2025-06-20 18:36:33 -0600

    Update install.sh
    added line 9 because running the script in its current state results in 'Command 'npm' not found, but can be installed with: apt install npm' error

--- ops/install.sh
+++ ops/install.sh
@@ -5,7 +5,8 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
-sudo apt-get install -y nodejs npm
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
 
 echo "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/openresty.list
 
